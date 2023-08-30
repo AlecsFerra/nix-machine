8bb200c7811d21eda7b379927411d3860e9dcb06
AlecsFerra <alecsferra@protonmail.com>
2023-08-30 15:27:06 +0200

    firefox addons

--- nixos/user/firefox.nix
+++ nixos/user/firefox.nix
@@ -2,9 +2,48 @@
 {
   programs.firefox = {
     enable = true;
+    
+    package = pkgs.wrapFirefox pkgs.firefox-unwrapped {
+      extraPolicies = {
+        CaptivePortal = false;
+        DisableFirefoxStudies = true;
+        DisablePocket = true;
+        DisableTelemetry = true;
+        DisableFirefoxAccounts = false;
+        NoDefaultBookmarks = true;
+        OfferToSaveLogins = false;
+        OfferToSaveLoginsDefault = false;
+        PasswordManagerEnabled = false;
+        FirefoxHome = {
+          Search = true;
+          Pocket = false;
+          Snippets = false;
+          TopSites = false;
+          Highlights = false;
+        };
+        UserMessaging = {
+          ExtensionRecommendations = false;
+          SkipOnboarding = true;
+        };
+      };
+    };
+
     profiles.alecs = {
       extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+        # Needed
+        # umatrix
+        ublock-origin
+        # bypass-paywalls-clean
+        consent-o-matic
+        facebook-container
+        # Usability
+        vimium
+        darkreader
       ];
+
+      settings = {
+        "browser.startup.homepage" = "https://calendar.google.com/calendar/u/0/r";
+      };
     };
   };
 }
