161d5adaf847fd5b21b9fd5d031585486378cda5
AlecsFerra <alecsferra@protonmail.com>
2023-10-20 12:01:15 +0200

    Shell: made aliases shell independent

--- nixos/user/shell/default.nix
+++ nixos/user/shell/default.nix
@@ -1,4 +1,21 @@
 { pkgs, ... }:
 {
   imports = [ ./zsh.nix ];
+
+  home.shellAliases = {
+    tb = "nc termbin.com 9999";
+    ":q" = "exit";
+    v = "$EDITOR";
+    s = "sudo ";
+    sv = "sudoedit";
+    znix-shell = "nix-shell --run zsh";
+    pkg-repl = "nix repl --file '<nixpkgs/nixos>'";
+    rebuild-switch = "sudo nixos-rebuild switch";
+    tmp = "cd $(mktemp -d)";
+    inhibit = "gnome-session-inhibit --inhibit idle";
+    open = "xdg-open";
+    patch = 
+      "patchelf --set-interpreter \"$(cat $NIX_CC/nix-support/dynamic-linker)\"";
+  };
+
 }
