38322a2546eae149a65345273dca094df733cad0
AlecsFerra <alecsferra@protonmail.com>
2023-10-24 20:10:58 +0200

    Eww: battery charging

--- nixos/user/desktop/statusbar/eww/system.nix
+++ nixos/user/desktop/statusbar/eww/system.nix
@@ -7,26 +7,42 @@ in
   config = mkIf cfg.eww.enable {
     xdg.configFile = {
       "eww/system.yuck".text = ''
-        (defvar battery_charging    `["󰢟", "󰢜", "󰂆", "󰂇", "󰂈", "󰢝", "󰂉", "󰢞", "󰂊", "󰂋", "󰂄"]`)
-        (defvar battery_discharging `["󰂎", "󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"]`)
+        (defvar battery_icons `["󰂎", "󰁺", "󰁻", "󰁼", "󰁽", "󰁾", "󰁿", "󰂀", "󰂁", "󰂂", "󰁹"]`)
 
+        (defwidget battery-icon []
+          (label
+            :angle 270
+            :class "battery-icon ''${EWW_BATTERY.total_avg > 20 ? ''' : 'low'}"
+            :tooltip "Battery on ''${round(EWW_BATTERY.total_avg,0)}% 
+                      (''${EWW_BATTERY.BAT0.status})"
+            :text {battery_icons[
+                    round(EWW_BATTERY.total_avg / 
+                          (100 / arraylength(battery_icons)) - 1,0)]}))
+
+        (defwidget charging-icon []
+          (label
+            :class "battery-bolt"
+            :tooltip "Battery on ''${round(EWW_BATTERY.total_avg,0)}% 
+                      (''${EWW_BATTERY.BAT0.status})"
+            :text {EWW_BATTERY.BAT0.status == "Charging" ? "󱐋" : ""}))
+        
         (defwidget battery []
-          (box
+          (overlay
             :class "battery"
-            (label
-              :angle 270
-              :class "icon ''${EWW_BATTERY.total_avg > 20 ? ''' : 'low'}"
-              :tooltip "battery on ''${round(EWW_BATTERY.total_avg,0)}% (''${EWW_BATTERY.BAT0.status})"
-              :text {(EWW_BATTERY.BAT0.status =='Charging' ?
-                battery_charging : 
-                battery_discharging)
-                  [round(EWW_BATTERY.total_avg / (100 / arraylength(battery_charging)) - 1,0)]})))
+            (battery-icon)
+            (charging-icon)))
       '';
 
       "eww/system.scss".text = with config.lib.stylix.colors; ''
-        .battery {
+        .battery-icon {
           padding-right: 0px;
           font-size: 28px;
+        }
+
+        .battery-bolt {
+          padding-right: 0px;
+          font-size: 16px;
+          color: #${base04};
         }
 
         .battery .low {
